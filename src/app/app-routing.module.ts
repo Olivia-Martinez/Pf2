28a220681ec56a543ee1f988a429c54b64af4f91
Olivia-Martinez <olive.mtz@outlook.com>
2020-06-29 20:16:13 -0500

    Templates pero falta dispositivo ymonitor y no se ve aun en en navegador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
+import { DispositivoComponent } from './components/dispositivo/dispositivo.component';
+import { MonitorComponent } from './components/monitor/monitor.component';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DispositivoComponent}, // '/' Mostrará el componente de los datos de los dispositivos
+  { path: '/monitor', component: MonitorComponent}, // '/monitor' Mostrara el componente monitor en la vista
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { DispositivoComponent } from './components/dispositivo/dispositivo.component';
+import { MonitorComponent } from './components/monitor/monitor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    BodyComponent
+    BodyComponent,
+    PanelComponent,
+    DispositivoComponent,
+    MonitorComponent
   ],
   imports: [
     BrowserModule,
